12dc9947c2ae13487149b18cc3918bd7fff7583a
Mateo González <mateogonz1999@hotmail.com>
2026-04-13 14:51:17 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ def hello_world():
     return jsonify({"mensaje": "¡Hola! Esta es una aplicación de ejemplo para CI/CD"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
